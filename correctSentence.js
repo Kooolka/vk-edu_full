c86ef90a0cf0ed505024af40aafa3371b7a5810c
Kooolka <semonaevnikolaj3@gmail.com>
2025-10-19 02:11:12 +0300

    gitignire and second ex

---
+++ correctSentence.js
@@ -0,0 +1,35 @@
+/*
+For the input of your function, you will be given one sentence.
+You have to return a corrected version,
+that starts with a capital letter and ends with a period (dot).
+
+Example:
+
+input (string): "hey, friend"
+output (string): "Hey, friend."
+
+Updated first 'h' to 'H', added '.'.
+
+More examples:
+
+correctSentence("greetings, friends") == "Greetings, friends."
+correctSentence("Greetings, friends") == "Greetings, friends."
+correctSentence("Greetings, friends.") == "Greetings, friends."
+ */
+
+export default function correctSentence(text) {
+var s = text.split('')
+if (s[0] == s[0].toUpperCase()) {
+  if (s[s.length-1] == ".") {
+    return s.join('')  } else {
+    s.push(".")
+    return s.join('')  }
+} else {
+  s[0] = s[0].toUpperCase()
+  if (s[s.length-1] == ".") {
+    return s.join('')  } else {
+    s.push(".")
+    return s.join('')
+  }
+}
+}
